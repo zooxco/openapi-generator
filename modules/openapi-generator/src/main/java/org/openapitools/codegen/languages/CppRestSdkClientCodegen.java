8abc846896c7429b646873064a5cab3ca33263e1
Daniel Miller <daniel@zoox.com>
2018-07-16 15:26:43 -0700

    Restore default supporting files functionality

--- modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/CppRestSdkClientCodegen.java
+++ modules/openapi-generator/src/main/java/org/openapitools/codegen/languages/CppRestSdkClientCodegen.java
@@ -169,11 +169,17 @@ public class CppRestSdkClientCodegen extends AbstractCppCodegen {
         updateImportMapping();
     }
 
-    private String supportingFilePath(String filename) {
+    private String supportingFilePath(String filename, String defaultPath) {
         if(!supportingFilesDirectory.isEmpty()) {
             return Paths.get(supportingFilesDirectory, filename).toString();
+        } else if(!defaultPath.isEmpty()) {
+            return Paths.get(defaultPath, filename).toString();
         }
         return filename;
+    }
+
+    private String supportingFilePath(String filename) {
+        return supportingFilePath(filename, "");
     }
 
     private void updateImportMapping() {
@@ -182,7 +188,7 @@ public class CppRestSdkClientCodegen extends AbstractCppCodegen {
         importMapping.put("std::string", globalInclude("string"));
         importMapping.put("utility::string_t", globalInclude("cpprest/details/basic_types.h"));
         importMapping.put("utility::datetime", globalInclude("cpprest/details/basic_types.h"));
-        importMapping.put("HttpContent", quoteInclude(supportingFilePath("HttpContent.h")));
+        importMapping.put("HttpContent", quoteInclude(supportingFilePath("HttpContent.h", "../")));
         importMapping.put("ModelBase", quoteInclude(supportingFilePath("ModelBase.h")));
         importMapping.put("Object", quoteInclude(supportingFilePath("Object.h")));
     }
@@ -208,7 +214,7 @@ public class CppRestSdkClientCodegen extends AbstractCppCodegen {
         }
 
         // Ensure that the supportingFilesNamespace always ends with "::"
-        if(!supportingFilesNamespace.endsWith("::")) {
+        if(!supportingFilesNamespace.isEmpty() && !supportingFilesNamespace.endsWith("::")) {
             supportingFilesNamespace += "::";
         }
 
@@ -220,9 +226,9 @@ public class CppRestSdkClientCodegen extends AbstractCppCodegen {
         additionalProperties.put("apiHeaderGuardPrefix", apiPackage.replaceAll("\\.", "_").toUpperCase());
         additionalProperties.put("declspec", declspec);
         additionalProperties.put("defaultInclude", defaultInclude);
-        additionalProperties.put("supportingFilesNamespace", supportingFilesNamespace);
         // Intentionally omitting:
         //  - supportingFilesDirectory
+        //  - additionalProperties.put("supportingFilesNamespace", supportingFilesNamespace);
         // These have context-dependant defaults in the individual templates
     }
 
